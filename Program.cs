a14748223636f169aad2c7fbe8b91779a054f15b
Elena <elena1709@yandex.ru>
2022-11-14 08:48:23 +0300

    commit for hw

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string Integers(int n)
+{
+    if (n < 1) return $"Число не натуральное ";
+    if (n == 1) return $"{1}";
+    else return $"{n}, " + Integers(n - 1);
+}
+
+Console.Clear();
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"N = {n} -> \"{Integers(n)}\"");
+
